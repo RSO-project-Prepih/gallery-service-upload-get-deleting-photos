07220c287fb5bc0358517045853b42a8a7a4f5e8
lovc21 <jakob.dekleva@gmail.com>
2023-10-31 23:27:38 +0100

    feat: add db connection

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/RSO-project-Prepih/gallery-service-uplode-get-deliting-photos.git
 
 go 1.20
+
+require github.com/joho/godotenv v1.5.1
+
+require github.com/lib/pq v1.10.9 // indirect
--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/RSO-project-Prepih/gallery-service-uplode-get-deliting-photos.git/database"
 )
 
 func main() {
 	fmt.Println("Hello World")
+	database.TestConnection()
+
 }
