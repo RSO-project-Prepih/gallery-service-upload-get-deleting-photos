050096c440011782974117d3e5a79d39ad257676
lovc21 <jakob.dekleva@gmail.com>
2024-01-10 18:38:20 +0100

    fix: grpc addres

--- handlers/post_photos.go
+++ handlers/post_photos.go
@@ -84,7 +84,7 @@ func UploadPhoto(c *gin.Context) {
 	}
 	log.Printf("Rows affected: %d", rowsAffected)
 
-	grpcAddress := "img-info-service.default.svc.cluster.local:50051" // Update with actual address if different
+	grpcAddress := "35.233.124.151:5000" // Update with actual address if different
 	photoServiceClient, err := grpcclient.NewPhotoServiceClient(grpcAddress)
 
 	if err != nil {
@@ -101,21 +101,19 @@ func UploadPhoto(c *gin.Context) {
 	})
 
 	log.Printf("Response: %v", response)
-	// temporary solution for frontend
-	/*
-		if err != nil || !response.Allowed {
-			// Delete the image if the response is not allowed
-			_, delErr := db.Exec("DELETE FROM images WHERE image_id = $1", imageID)
-			if delErr != nil {
-				log.Printf("Failed to delete the photo: %v", delErr)
-				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete the photo"})
-				return
-			}
-			log.Printf("Photo deleted successfully")
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Photo not allowed"})
+
+	if err != nil || !response.Allowed {
+		// Delete the image if the response is not allowed
+		_, delErr := db.Exec("DELETE FROM images WHERE image_id = $1", imageID)
+		if delErr != nil {
+			log.Printf("Failed to delete the photo: %v", delErr)
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete the photo"})
 			return
 		}
-	*/
+		log.Printf("Photo deleted successfully")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Photo not allowed"})
+		return
+	}
 
 	log.Printf("Photo uploaded successfully. Rows affected: %d", rowsAffected)
 
